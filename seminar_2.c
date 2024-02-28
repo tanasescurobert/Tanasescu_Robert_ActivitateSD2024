1cd58a24aaad0a8c69678fddd0d2a8b6465be423
Tanasescu Robert <161322191+tanasescurobert@users.noreply.github.com>
2024-02-28 10:16:43 +0200

    Update seminar_2.c

--- seminar_2.c
+++ seminar_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
 
 struct Avion {
 	int nrPers;
@@ -9,11 +10,13 @@ struct Avion {
 
 
 struct Avion initializare(int nrPers, const char* companie, float pretBilet) {
-	
+
 	struct Avion avion;
 	avion.nrPers = nrPers;
-	avion.companie = (char*)malloc(strlen(companie) + 1);     
-	strcpy(avion.companie, companie);    
+	
+	avion.companie = (char*)malloc(strlen(companie) + 1);
+	strcpy_s(avion.companie, strlen(companie) + 1, companie); 
+	
 	avion.pretBilet = pretBilet;
 
 	return avion;
@@ -40,7 +43,7 @@ struct Avion* copiazaPrimeleNAvioane(struct Avion* avioane, int nrAvioane, int n
 	if (nrAvioaneCopiate < nrAvioane && nrAvioaneCopiate > 0)
 	{
 		struct Avion* avioaneCopiate = (struct Avion*)malloc(sizeof(struct Avion) * nrAvioaneCopiate);
-		for (int i = 0; i, nrAvioaneCopiate; i++) {
+		for (int i = 0; i < nrAvioaneCopiate; i++) {
 			avioaneCopiate[i] = initializare(avioane[i].nrPers, avioane[i].companie, avioane[i].pretBilet);
 		}
 		return avioaneCopiate;
@@ -53,7 +56,7 @@ void dezalocare(struct Avion** avioane, int* nrAvioane) {
 
 	if ((*avioane) != NULL)
 	{
-		for (int i = 0; i, nrAvioane; i++)
+		for (int i = 0; i < *nrAvioane; i++)
 		{
 			free((*avioane)[i].companie);
 		}
@@ -66,21 +69,21 @@ void dezalocare(struct Avion** avioane, int* nrAvioane) {
 
 
 void copiazaAvioaneIeftine(struct Avion* avioane, int nrAvioane, float pragPret, struct Avion** avioaneNoi, int* dimNoua) {
-	
-	if (avioaneNoi != NULL)	
+
+	if (*avioaneNoi != NULL)
 		dezalocare(avioaneNoi, dimNoua);
-	else 
+	else
 		*dimNoua = 0;
-	
+
 	for (int i = 0; i < nrAvioane; i++) {
 		if (avioane[i].pretBilet < pragPret) {
 			(*dimNoua)++;
 		}
 	}
+	 
+	*avioaneNoi = (struct Avion*)malloc(sizeof(struct Avion) * (*dimNoua));
 
-	avioaneNoi = (struct Avion*)malloc(sizeof(struct Avion) * (*dimNoua));
-	
-	int index = 0; 
+	int index = 0;
 	for (int i = 0; i < nrAvioane; i++) {
 		if (avioane[i].pretBilet < pragPret)
 		{
@@ -97,10 +100,10 @@ struct Avion getAvionDupaCompanie(struct Avion* avioane, int dim, const char* co
 		for (int i = 0; i < dim; i++) {
 			if (avioane[i].companie != NULL)
 			{
-				if (strcmp(avioane[i].companie, companie) == 0) 
+				if (strcmp(avioane[i].companie, companie) == 0)
 					return initializare(avioane[i].nrPers, avioane[i].companie, avioane[i].pretBilet);
 			}
-			
+
 		}
 	}
 
@@ -112,38 +115,39 @@ struct Avion getAvionDupaCompanie(struct Avion* avioane, int dim, const char* co
 void main() {
 
 	int nrAvioane = 5;
-	struct Avion* avioane = (struct Avion*)malloc(sizeof(struct Avion) * nrAvioane);  
-	for (int i = 0; i, nrAvioane; i++) {
+	struct Avion* avioane = (struct Avion*)malloc(sizeof(struct Avion) * nrAvioane);
+	for (int i = 0; i < nrAvioane; i++) {
 		avioane[i] = initializare(i + 50, "Tarom", 10 * i + 40);
-	}	
+	}
 	afisareVectorAvioane(avioane, nrAvioane);
 
 
 
-	int nrAvioaneCopiate = 3; 
-	struct Avion* avioaneCopiate = copiazaPrimeleNAvioane(avioane, nrAvioane, nrAvioaneCopiate); 
-	printf("\n\n"); 
-	afisareVectorAvioane(avioaneCopiate, nrAvioaneCopiate); 
+	int nrAvioaneCopiate = 3;
+	struct Avion* avioaneCopiate = copiazaPrimeleNAvioane(avioane, nrAvioane, nrAvioaneCopiate);
+	printf("\nCopiere primele %d avioane:\n", nrAvioaneCopiate);
+	afisareVectorAvioane(avioaneCopiate, nrAvioaneCopiate);
 
 
 
-	struct Avion* avioaneIeftine = NULL; 
-	int nrAvioaneIeftine = 0; 
-	avioane[4].pretBilet = 30;  
-	copiazaAvioaneIeftine(avioane, nrAvioane, 50, &avioaneIeftine, &nrAvioaneIeftine);  
-	afisareVectorAvioane(avioaneIeftine, nrAvioaneIeftine); 
-	 
-
-
-	struct Avion avion = getAvionDupaCompanie(avioane, nrAvioane, "Tarom"); 
-	printf("Avion cautat: \n"); 
-	afisareAvion(avion); 
+	struct Avion* avioaneIeftine = NULL;
+	int nrAvioaneIeftine = 0;
+	avioane[4].pretBilet = 30;
+	copiazaAvioaneIeftine(avioane, nrAvioane, 50, &avioaneIeftine, &nrAvioaneIeftine);
+	printf("\nAfisare avioane ieftine(sub %d RON):\n", 50);
+	afisareVectorAvioane(avioaneIeftine, nrAvioaneIeftine);
 
 
 
-	dezalocare(&avioane, &nrAvioane); 
-	dezalocare(&avioaneCopiate, &nrAvioaneCopiate); 
-	dezalocare(&avioaneIeftine, &nrAvioaneIeftine); 
+	struct Avion avion = getAvionDupaCompanie(avioane, nrAvioane, "Tarom");
+	printf("\nAvion cautat: \n");
+	afisareAvion(avion);
+
+	dezalocare(&avioaneCopiate, &nrAvioaneCopiate);
+	dezalocare(&avioaneIeftine, &nrAvioaneIeftine);
 	free(avion.companie);
 
-}
+	dezalocare(&avioane, &nrAvioane);
+		
+
+} 
